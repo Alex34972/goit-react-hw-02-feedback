37c044088524b047e6d04f4ee66142369b6645a2
Alex34972 <alex_hifi@ukr.net>
2021-07-28 19:00:44 +0300

    Update Statistics.js

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <div className="statistics">
@@ -12,5 +12,12 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
       </p>
     </div>
   );
+};
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
 export default Statistics;
